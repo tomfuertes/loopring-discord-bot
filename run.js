4527385ce496cb7145e79cab4f2d352b11f4b692
Tom Fuertes <tomfuertes@gmail.com>
2022-06-05 16:24:58 -0500

    update skipper

--- run.js
+++ run.js
@@ -122,7 +122,11 @@ const processBlock = async (id) => {
   for (const nft of nfts) {
     if (nft.id === '21866-217') continue; // abort a rando large request
     if (nft.id === '22211-27') continue; // abort a rando large request
-    if (nft.minter === '0x9fb516b06de9d12915785fb555b3d69dc83d0383') continue; // abort a rando large request
+    if (
+      nft.minter &&
+      nft.minter.address === '0x9fb516b06de9d12915785fb555b3d69dc83d0383'
+    )
+      continue; // abort a rando large request
 
     console.log('nft', {
       l2: nft.id,
